14bf8ed966ffccf44c2948862bdefa6e59692101
Dratr <dratr@github.github>
2016-07-22 18:48:32 -0400

    Don't wildly oscillate maps and spam map setting when doing hotspot movements     To test: open world map, mouse over different zones in carb map

--- Carbonite/NxMap.lua
+++ Carbonite/NxMap.lua
@@ -5839,7 +5839,7 @@ function Nx.Map:CheckWorldHotspotsType (wx, wy, quad)
 	for n, spot in ipairs (quad) do
 		if wx >= spot.WX1 and wx <= spot.WX2 and wy >= spot.WY1 and wy <= spot.WY2 then
 
-			local curId = self:GetCurrentMapId()
+			local curId = self:GetRealMapId()
 
 			if spot.MapId ~= curId then
 
@@ -8650,7 +8650,7 @@ function Nx.Map:SetCurrentMap (mapId)
 					local cont = self.MapWorldInfo[mapId].Cont
 					local zone = self.MapWorldInfo[mapId].Zone
 
-					if not self.MapWorldInfo[mapId].City and (not cont or not zone or mapId == self:GetRealBaseMapId() or mapId == self:GetRealMapId()) then						
+					if not self.MapWorldInfo[mapId].City and (not cont or not zone or mapId == self:GetRealBaseMapId()) then
 						SetMapToCurrentZone()		-- This fixes the Scarlet Enclave map selection, so we get player position						
 						SetDungeonMapLevel (1)
 					else
