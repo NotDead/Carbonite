8081d35bbf4f9beb0d687fb9c86078ced4dae596
dratr <dratr@github>
2016-08-05 17:02:09 -0400

    Bugged zones now automatically full/no transparancy on minimap. Fixed some logic with the toggling of fullsize/docked Remove prints

--- Carbonite/NxMap.lua
+++ Carbonite/NxMap.lua
@@ -2211,10 +2211,10 @@ function Nx.Map:MinimapUpdate()
 			zoomType = 0
 		end
 		if (indoorChange and Nx.db.profile.MiniMap.IndoorTogFullSize) or (bugChange and Nx.db.profile.MiniMap.BuggedTogFullSize) then
-print("Indoorchange:", indoorChange," City? ", info.City, "Indoor? ", indoors)
-print("Bugchange:", bugChange," bug? ", bugged)
+			--Nx.prt("Indoorchange:", indoorChange," City? ", info.City, "Indoor? ", indoors)
+			--Nx.prt("Bugchange:", bugChange," bug? ", bugged)
 			lOpts.NXMMFull = false
-			if indoors or bugged then
+			if (indoors and Nx.db.profile.MiniMap.IndoorTogFullSize) or (bugged and Nx.db.profile.MiniMap.BuggedTogFullSize) then
 				lOpts.NXMMFull = true
 			end
 			self.MMMenuIFull:SetChecked (lOpts.NXMMFull)
